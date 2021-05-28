fd0d49499967ea4947f04338c96488b8a8228dfc
Shakime <jeykimers.johnson@gmail.com>
2021-05-28 06:52:31 -0600

    [Add] Creación, llamada y modificación de listas.
    Pendiente insertar y eliminar.

--- Project/parser/parser.py
+++ Project/parser/parser.py
@@ -26,13 +26,10 @@ TODO:
 
 # Diccionarios
 functions = {}  # Lista de funciones almacenadas del programa.
-variables = {}  # Las variables almacenadas se guardarán como {ID: valor}.
+variables = {"a": [1, 2, 3, 4]}  # Las variables almacenadas se guardarán como {ID: valor}.
 
-# Lista de errores del programa
+# Lista de errores del programa.
 errors_list = []
-
-ids_list = []
-values_list = []
 
 # Orden para asignar a las operaciones.
 # Se utiliza el %prec para la declaracion de expresiones negativas.
@@ -44,7 +41,7 @@ precedence = (
     ('right', 'EXPONENTE')
 )
 
-''' GRAMMARS '''
+''' %%%%%%%%%%%%%%%%%%%%%%%%%%%%#################  GRAMMAR RULES %%%%%%%%%%%%%%%%%%%%%%%%%%%%#################'''
 
 '''Cada regla gramatical está definida por una función de Python donde la cadena de documentación 
 de esa función contiene la especificación gramatical libre de contexto apropiada.
@@ -70,7 +67,7 @@ def p_statments(p):
         p[0] = p[1] + [p[2]]
 
 
-# Definición de posibles expresiones
+# Definición de posibles expresiones.
 def p_sentencia_expr(p):
     ''' statement : expression
                     | print
@@ -78,49 +75,71 @@ def p_sentencia_expr(p):
     p[0] = p[1]
 
 
+# Expresion vacia
+def p_empty(p):
+    """empty :"""
+    pass
+
+
+# Expresion para imprimir las variables presentes.
 def p_printVariables(p):
     """
     print : PUNTO
     """
-    print(variables)
+    print(" ▶ variables: ", variables)
+    print(" ▶ errors: ", errors_list)
 
 
 # Expresion para la declaracion de una variable.
 def p_statement_assign(p):
     """
-        statement : ids IGUAL values
+        statement : ID IGUAL value
+                  | ids IGUAL params
     """
-    p[0] = multiple_assign_aux()
-    ids_list.clear()
-    values_list.clear()
+
+    if isinstance(p[1], str):  # Si es solo un id
+        p[0] = assign_aux(p[1], p[3])
+    else:  # Para más de un id
+        p[0] = multiple_assign_aux(p[1], p[3])
     print(" ▶ variables: ", variables)
 
 
+# Expresion para definir multiples ID o uno solo.
 def p_ids(p):
-    """ ids : ID COMA ids
-            | ID
+    """ ids : ID
+            | ID COMA ids
+
     """
-    ids_list.append(p[1])
+    if len(p) == 2:  # Si es solo un id
+        p[0] = [p[1]]
+    else:  # Para más de un id
+        p[0] = [p[1]] + p[3]
 
 
-def p_values(p):
+# Expresion para obtenr parametros (se dan en forma de una lista)
+def p_params(p):
     """
-    values : value COMA values
-           | value
+    params  : value
+            | params COMA params
     """
-    values_list.append(p[1])
+    if len(p) == 2:  # Si es solo un parametro
+        p[0] = [p[1]]
+    else:  # Si son más de dos parametros
+        p[0] = p[1] + p[3]
 
 
-def multiple_assign_aux():
+# Funcion auxiliar para declarar multiples variables.
+# Hace uso de assign_aux para declarar una sola variable a la vez.
+def multiple_assign_aux(ids_list, values_list):
     if len(ids_list) != len(values_list):
-        print("ERROR in line {0}! The number of values does not match the number of IDs.".format("LINE_NUMBER"))
+        errors_list.append(
+            "ERROR in line {0}! The number of values does not match the number of IDs.".format("LINE_NUMBER"))
     var_list = []
-    ids_list.reverse()
-    values_list.reverse()
 
     for id in ids_list:
         if id in variables.keys():
-            print("ERROR in line {2}! \"{0}\" is already defined as {1}.".format(id, variables.get(id), "LINE_NUMBER"))
+            errors_list.append(
+                "ERROR in line {2}! \"{0}\" is already defined as {1}.".format(id, variables.get(id), "LINE_NUMBER"))
             return None
 
     for i in range(len(ids_list)):
@@ -129,14 +148,24 @@ def multiple_assign_aux():
     return var_list
 
 
+# Funcion auxiliar para declarar una variable.
 def assign_aux(id, value):
     if id in variables:
         if equalsType(variables.get(id), value):
-            return getVariable(id, value)
+            setVariable(id, value)
+            return getVariable(id)
         else:
-            print("ERROR in line {2}! \"{0}\" is already type {1}.".format(id, type(value), "LINE_NUMBER"))
+            tipo = type(value)
+            if value: tipo = list
+            print("ERROR in line {2}! \"{0}\" is already {1}.".format(id, tipo, "LINE_NUMBER"))
     else:
-        return getVariable(id, value)
+        setVariable(id, value)
+        return getVariable(id)
+
+
+# Valida si una variable es del tipo del segundo parametro de entrada.
+def ifType(var1, tipo):
+    return type(var1) == tipo
 
 
 # Valida si dos variables son del mismo tipo.
@@ -145,10 +174,14 @@ def equalsType(var1, var2):
 
 
 # Guarda la variable en el diccionario.
-def getVariable(id, value):
+def setVariable(id, value):
     if value is not None:
         variables[id] = value
-        return value
+
+
+# Get de la variable en el diccionario.
+def getVariable(id):
+    return variables.get(id, None)  # Retorna none en caso de que no exista el key.
 
 
 # Expresion para la determinar el valor de una variable.
@@ -157,7 +190,7 @@ def p_value(p):
               | BOOLEAN
               | ID
    """
-    if isVariable(p[1], "LINE_NUMBER"):
+    if isDeclared(p[1], "LINE_NUMBER"):
         p[0] = variables.get(p[1])
     else:
         p[0] = p[1]
@@ -168,24 +201,24 @@ def p_type(p):
     """
         statement : TYPE PARENTESISIZQ ID PARENTESISDER
     """
-    if isVariable(p[3], "LINE_NUMBER"):
+    if isDeclared(p[3], "LINE_NUMBER"):
         var = variables.get(p[3])
         if isinstance(var, bool):
-            p[0] = 'bool'
+            p[0] = bool
         elif isinstance(var, int):
-            p[0] = 'int'
+            p[0] = int
         else:
             print("ERROR in type!")
 
 
 # Verifica si ID existe en el diccionario.
-def isVariable(var, line):
+def isDeclared(var, line):
     # Si el valor es una instancia de ID.
     if not isinstance(var, str):
         return False
     # Si no existe el ID dentro del diccionario, return false.
     if variables.get(var) is None:
-        print("ERROR in line {1}! \"{0}\" is not yet defined.".format(var, line))
+        errors_list.append("ERROR in line {1}! \"{0}\" is not yet defined.".format(var, line))
         return False
     return True
 
@@ -195,7 +228,133 @@ def isVariable(var, line):
 #     """ statement : NAME '(' NAME ')' '=' expression """
 #     p[0] = p[1]
 
-''' ARITMETICA '''
+''' %%%%%%%%%%%%%%%%%%%%%%%%%%%%  LISTS  %%%%%%%%%%%%%%%%%%%%%%%%%%%% '''
+
+
+# Expresion para crear una lista, ya sea vacia o con parámetros.
+def p_statement_list_assign(p):
+    """ statement  :  ID IGUAL CORCHETEIZQ params CORCHETEDER
+                   |  ID IGUAL CORCHETEIZQ empty CORCHETEDER
+    """
+    if p[4] is None:
+        p[0] = assign_aux(p[1], [])
+    else:
+        if not checkTypeInList_aux("LINE_NUMBER", p[4], type(p[4][0])):  # Si la lista tiene distintos tipos, no se hace nada.
+            return
+        p[0] = assign_aux(p[1], p[4])
+
+
+# Expresion para obtener o setear el valor en las posiciones del indice recibido de una lista.
+def p_statement_list_index(p):
+    """ statement  :  ID index
+                   |  ID index IGUAL params
+                   |  ID index IGUAL CORCHETEIZQ params CORCHETEDER
+    """
+    if not isDeclared(p[1], "LINE_NUMBER"):  # Revisar antes si existe la variable.
+        return
+
+    id = p[1]
+    size = len(p)
+    lst = getVariable(id)
+    i, j = p[2][0], p[2][1]
+
+    if not checkIndexRange_aux("LINE_NUMBER", lst, i):
+        return  # Si el index no es valido, no hace nada.
+
+    if size == 3:
+        if j is None:  # Si es la llamada de un indice solo.
+            p[0] = lst[i]
+        else:  # Si es la llamada de un rango
+            p[0] = lst[i:j]
+
+    ## ASIGNACION
+    var = getVariable(id)
+
+    if not ifType(var, list):
+        errors_list.append("TypeError in line {1}: \'{0}\' is not subscriptable."
+                           .format(type(var), "LINE_NUMBER"))
+
+    if size == 5:
+        if not checkListValidations_aux("LINE_NUMBER", p[4], var, i):  # validaciones antes del cambio.
+            return
+        p[0] = var[i] = p[4][0]  # actualizar variable y salida.
+    elif size == 7:
+        if not checkListValidations_aux("LINE_NUMBER", p[5], var, i, j):  # validaciones antes del cambio.
+            return
+        p[0] = var[i:j] = p[5]  # actualizar variable y salida.
+
+
+# Expresion para obtener limite inicial y final de los indices dentro de corchetes.
+def p_statemente_dospuntos(p):
+    """ index  : CORCHETEIZQ expression CORCHETEDER
+               | CORCHETEIZQ expression DOSPUNTOS expression CORCHETEDER
+    """
+    if len(p) == 4:
+        lst = [p[2]] + [None]
+    else:
+        lst = [p[2]] + [p[4]]
+    p[0] = lst
+
+
+# Funcion auxiliar para realizar varias validaciones a la vez.
+def checkListValidations_aux(line, param, var, i, j=None):
+    if checkRangeConcordance_aux(line, param, i, j) and \
+            checkTypeInList_aux(line, param, type(var[0])) and \
+            checkIndexRange_aux(line, var, i, j):
+        return True
+    return False
+
+
+# Función auxiliar para validar que todos los elementos de una lista corresponden al mismo tipo.
+def checkTypeInList_aux(line, lst, tipo):
+    if not lst:
+        return True
+    else:
+        for i in lst:
+            if type(i) != tipo:
+                errors_list.append("ERROR in line {1}: \"{0}\" type does not match the type of elements in the list."
+                                   .format(i, line))
+                return False
+    return True
+
+
+# Funcion para verificar que un indice se encuentra dentro del rango indicado.
+def checkIndexRange_aux(line, lst, index1, index2=None):
+    if index1 >= len(lst):
+        errors_list.append("ERROR in line {1}: Index \"{0}\" out of range."
+                           .format(index1, line))
+        return False
+    elif (index2 is not None) and (index2 > len(lst)):
+        errors_list.append("ERROR in line {1}: Index \"{0}\" out of range."
+                           .format(index2, line))
+        return False
+    else:
+        return True
+
+
+# Funcion auxiliar para verificar la cantidad de parametros coincide con los indices.
+def checkRangeConcordance_aux(line, params, i, j):
+    if (j is None) and (len(params) == 1):
+        return True
+    elif len(params) == (j - i):
+        return True
+    else:
+        errors_list.append("IndexError in line {0}: The number of parameters does not match that of the indexes."
+                           .format(line))
+        return False
+
+
+# Expresion para crear lista con range.
+def p_statement_list_range(p):
+    """ statement : ID IGUAL LIST PARENTESISIZQ RANGE PARENTESISIZQ INT COMA value PARENTESISDER PARENTESISDER """
+    tmp = []
+    for i in range(p[7]):
+        tmp.append(p[9])
+    p[0] = assign_aux(p[1], tmp)
+
+
+''' %%%%%%%%%%%%%%%%%%%%%%%%%%%%  ARITHMETIC OPERATIONS  %%%%%%%%%%%%%%%%%%%%%%%%%%%% '''
+
 
 # Expresiones entre paréntesis
 def p_expression_op(p):
@@ -232,15 +391,15 @@ def p_expression_op(p):
     elif p[2] == '%':
         p[0] = p[1] % p[3]
 
-    # Potencia
+    # Exponente
     elif p[2] == '^':
         p[0] = pow(p[1], p[3])
 
 
 # Número negativo
 def p_expression_uminus(p):
-   'expression : RESTA expression %prec UMENOS'
-   p[0] = -p[2]
+    'expression : RESTA expression %prec UMENOS'
+    p[0] = -p[2]
 
 
 # Expresiones entre paréntesis
@@ -248,35 +407,27 @@ def p_factor_expr(p):
     'expression : PARENTESISIZQ expression PARENTESISDER'
     p[0] = p[2]
 
+
 # Expresion para definir un término.
 def p_expression_num(p):
     'expression : INT'
     p[0] = p[1]
 
 
-
-
-
-
-
-''' ERRORS '''
+''' %%%%%%%%%%%%%%%%%%%%%%%%%%%%  ERRORS  %%%%%%%%%%%%%%%%%%%%%%%%%%%% '''
 
 
 # Error rule for syntax errors.
 def p_error(p):
     # print(p)
-    print("Syntax error in input: ", p.type)
+    print("✘ Syntax error in input")
     parser.errok()
-    # Reinicia el parser
-    parser.restart()
+    # # Reinicia el parser
+    # parser.restart()
 
 
 # Build the parser.
 parser = yacc.yacc()
-
-# parser.parse('a=1'
-#              'b=2'
-#              'a=b')
 
 while True:
     while True:
@@ -287,6 +438,6 @@ while True:
 
         answer = parser.parse(question)
         if answer is not None:
-            print(answer[0])
+            print("Result: ", answer[0])
 
     ################################## MAIN #################################
--- Project/parser/parsetab.py
+++ Project/parser/parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'leftSUMARESTAleftMULTIPLICACIONDIVISIONDIVISIONENTERArightUMENOSrightMODULOrightEXPONENTEBEGIN BLINK BOOLEAN CALL COMA CORCHETEDER CORCHETEIZQ DEL DELAY DIVISION DIVISIONENTERA DOSPUNTOS END EXPONENTE F FOR ID IF IGUAL IGUALES IN INSERT INT LEN LIST MAIN MAYORIGUAL MAYORQUE MENORIGUAL MENORQUE MIL MIN MODULO MULTIPLICACION NEG PARENTESISDER PARENTESISIZQ PRINTLED PRINTLEDX PROCEDURE PUNTO PYC RANGE RESTA SEG SHAPEC SHAPEF STEP STRING SUMA T TYPE statements : statements statement\n                   | statement\n     statement : expression\n                    | print\n    \n    print : PUNTO\n    \n        statement : ids IGUAL values\n     ids : ID COMA ids\n            | ID\n    \n    values : value COMA values\n           | value\n     value : INT\n              | BOOLEAN\n              | ID\n   \n        statement : TYPE PARENTESISIZQ ID PARENTESISDER\n    \n    expression   : expression RESTA expression\n                 | expression SUMA expression\n                 | expression DIVISIONENTERA expression\n                 | expression DIVISION expression\n                 | expression MULTIPLICACION expression\n                 | expression MODULO expression\n                 | expression EXPONENTE expression\n    expression : RESTA expression %prec UMENOSexpression : PARENTESISIZQ expression PARENTESISDERexpression : INT'
+_lr_signature = 'leftSUMARESTAleftMULTIPLICACIONDIVISIONDIVISIONENTERArightUMENOSrightMODULOrightEXPONENTEBEGIN BLINK BOOLEAN CALL COMA CORCHETEDER CORCHETEIZQ DEL DELAY DIVISION DIVISIONENTERA DOSPUNTOS END EXPONENTE F FOR ID IF IGUAL IGUALES IN INSERT INT LEN LIST MAIN MAYORIGUAL MAYORQUE MENORIGUAL MENORQUE MIL MIN MODULO MULTIPLICACION NEG PARENTESISDER PARENTESISIZQ PRINTLED PRINTLEDX PROCEDURE PUNTO PYC RANGE RESTA SEG SHAPEC SHAPEF STEP STRING SUMA T TYPE statements : statements statement\n                   | statement\n     statement : expression\n                    | print\n    empty :\n    print : PUNTO\n    \n        statement : ID IGUAL value\n                  | ids IGUAL params\n     ids : ID\n            | ID COMA ids\n\n    \n    params  : value\n            | params COMA params\n     value : INT\n              | BOOLEAN\n              | ID\n   \n        statement : TYPE PARENTESISIZQ ID PARENTESISDER\n     statement  :  ID IGUAL CORCHETEIZQ params CORCHETEDER\n                   |  ID IGUAL CORCHETEIZQ empty CORCHETEDER\n     statement  :  ID index\n                   |  ID index IGUAL params\n                   |  ID index IGUAL CORCHETEIZQ params CORCHETEDER\n     index  : CORCHETEIZQ expression CORCHETEDER\n               | CORCHETEIZQ expression DOSPUNTOS expression CORCHETEDER\n     statement : ID IGUAL LIST PARENTESISIZQ RANGE PARENTESISIZQ INT COMA value PARENTESISDER PARENTESISDER \n    expression   : expression RESTA expression\n                 | expression SUMA expression\n                 | expression DIVISIONENTERA expression\n                 | expression DIVISION expression\n                 | expression MULTIPLICACION expression\n                 | expression MODULO expression\n                 | expression EXPONENTE expression\n    expression : RESTA expression %prec UMENOSexpression : PARENTESISIZQ expression PARENTESISDERexpression : INT'
     
-_lr_action_items = {'TYPE':([0,1,2,3,4,10,11,12,24,25,26,27,28,29,30,31,32,33,34,35,36,38,41,42,],[6,6,-2,-3,-4,-24,-5,-1,-22,-15,-16,-17,-18,-19,-20,-21,-6,-10,-11,-12,-13,-23,-14,-9,]),'RESTA':([0,1,2,3,4,7,9,10,11,12,13,14,15,16,17,18,19,22,24,25,26,27,28,29,30,31,32,33,34,35,36,38,41,42,],[9,9,-2,13,-4,9,9,-24,-5,-1,9,9,9,9,9,9,9,13,-22,-15,-16,-17,-18,-19,-20,-21,-6,-10,-11,-12,-13,-23,-14,-9,]),'PARENTESISIZQ':([0,1,2,3,4,6,7,9,10,11,12,13,14,15,16,17,18,19,24,25,26,27,28,29,30,31,32,33,34,35,36,38,41,42,],[7,7,-2,-3,-4,21,7,7,-24,-5,-1,7,7,7,7,7,7,7,-22,-15,-16,-17,-18,-19,-20,-21,-6,-10,-11,-12,-13,-23,-14,-9,]),'INT':([0,1,2,3,4,7,9,10,11,12,13,14,15,16,17,18,19,20,24,25,26,27,28,29,30,31,32,33,34,35,36,38,40,41,42,],[10,10,-2,-3,-4,10,10,-24,-5,-1,10,10,10,10,10,10,10,34,-22,-15,-16,-17,-18,-19,-20,-21,-6,-10,-11,-12,-13,-23,34,-14,-9,]),'PUNTO':([0,1,2,3,4,10,11,12,24,25,26,27,28,29,30,31,32,33,34,35,36,38,41,42,],[11,11,-2,-3,-4,-24,-5,-1,-22,-15,-16,-17,-18,-19,-20,-21,-6,-10,-11,-12,-13,-23,-14,-9,]),'ID':([0,1,2,3,4,10,11,12,20,21,23,24,25,26,27,28,29,30,31,32,33,34,35,36,38,40,41,42,],[8,8,-2,-3,-4,-24,-5,-1,36,37,8,-22,-15,-16,-17,-18,-19,-20,-21,-6,-10,-11,-12,-13,-23,36,-14,-9,]),'$end':([1,2,3,4,10,11,12,24,25,26,27,28,29,30,31,32,33,34,35,36,38,41,42,],[0,-2,-3,-4,-24,-5,-1,-22,-15,-16,-17,-18,-19,-20,-21,-6,-10,-11,-12,-13,-23,-14,-9,]),'SUMA':([3,10,22,24,25,26,27,28,29,30,31,38,],[14,-24,14,-22,-15,-16,-17,-18,-19,-20,-21,-23,]),'DIVISIONENTERA':([3,10,22,24,25,26,27,28,29,30,31,38,],[15,-24,15,-22,15,15,-17,-18,-19,-20,-21,-23,]),'DIVISION':([3,10,22,24,25,26,27,28,29,30,31,38,],[16,-24,16,-22,16,16,-17,-18,-19,-20,-21,-23,]),'MULTIPLICACION':([3,10,22,24,25,26,27,28,29,30,31,38,],[17,-24,17,-22,17,17,-17,-18,-19,-20,-21,-23,]),'MODULO':([3,10,22,24,25,26,27,28,29,30,31,38,],[18,-24,18,18,18,18,18,18,18,18,-21,-23,]),'EXPONENTE':([3,10,22,24,25,26,27,28,29,30,31,38,],[19,-24,19,19,19,19,19,19,19,19,19,-23,]),'IGUAL':([5,8,39,],[20,-8,-7,]),'COMA':([8,33,34,35,36,],[23,40,-11,-12,-13,]),'PARENTESISDER':([10,22,24,25,26,27,28,29,30,31,37,38,],[-24,38,-22,-15,-16,-17,-18,-19,-20,-21,41,-23,]),'BOOLEAN':([20,40,],[35,35,]),}
+_lr_action_items = {'ID':([0,1,2,3,4,9,11,12,20,22,23,24,25,27,28,29,30,31,32,33,34,35,36,37,39,40,42,45,46,48,52,54,55,56,57,58,59,63,65,66,68,71,],[5,5,-2,-3,-4,-34,-6,-1,35,-19,43,35,47,-32,-25,-26,-27,-28,-29,-30,-31,-15,-7,35,-13,-14,35,-8,-11,-33,-22,-20,35,35,-16,-17,-18,-12,-23,-21,35,-24,]),'TYPE':([0,1,2,3,4,9,11,12,22,27,28,29,30,31,32,33,34,35,36,39,40,45,46,48,52,54,57,58,59,63,65,66,71,],[7,7,-2,-3,-4,-34,-6,-1,-19,-32,-25,-26,-27,-28,-29,-30,-31,-15,-7,-13,-14,-8,-11,-33,-22,-20,-16,-17,-18,-12,-23,-21,-24,]),'RESTA':([0,1,2,3,4,8,9,10,11,12,13,14,15,16,17,18,19,21,22,26,27,28,29,30,31,32,33,34,35,36,39,40,41,45,46,48,52,53,54,57,58,59,61,63,65,66,71,],[10,10,-2,13,-4,10,-34,10,-6,-1,10,10,10,10,10,10,10,10,-19,13,-32,-25,-26,-27,-28,-29,-30,-31,-15,-7,-13,-14,13,-8,-11,-33,-22,10,-20,-16,-17,-18,13,-12,-23,-21,-24,]),'PARENTESISIZQ':([0,1,2,3,4,7,8,9,10,11,12,13,14,15,16,17,18,19,21,22,27,28,29,30,31,32,33,34,35,36,38,39,40,45,46,48,52,53,54,57,58,59,60,63,65,66,71,],[8,8,-2,-3,-4,25,8,-34,8,-6,-1,8,8,8,8,8,8,8,8,-19,-32,-25,-26,-27,-28,-29,-30,-31,-15,-7,51,-13,-14,-8,-11,-33,-22,8,-20,-16,-17,-18,64,-12,-23,-21,-24,]),'INT':([0,1,2,3,4,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,24,27,28,29,30,31,32,33,34,35,36,37,39,40,42,45,46,48,52,53,54,55,56,57,58,59,63,64,65,66,68,71,],[9,9,-2,-3,-4,9,-34,9,-6,-1,9,9,9,9,9,9,9,39,9,-19,39,-32,-25,-26,-27,-28,-29,-30,-31,-15,-7,39,-13,-14,39,-8,-11,-33,-22,9,-20,39,39,-16,-17,-18,-12,67,-23,-21,39,-24,]),'PUNTO':([0,1,2,3,4,9,11,12,22,27,28,29,30,31,32,33,34,35,36,39,40,45,46,48,52,54,57,58,59,63,65,66,71,],[11,11,-2,-3,-4,-34,-6,-1,-19,-32,-25,-26,-27,-28,-29,-30,-31,-15,-7,-13,-14,-8,-11,-33,-22,-20,-16,-17,-18,-12,-23,-21,-24,]),'$end':([1,2,3,4,9,11,12,22,27,28,29,30,31,32,33,34,35,36,39,40,45,46,48,52,54,57,58,59,63,65,66,71,],[0,-2,-3,-4,-34,-6,-1,-19,-32,-25,-26,-27,-28,-29,-30,-31,-15,-7,-13,-14,-8,-11,-33,-22,-20,-16,-17,-18,-12,-23,-21,-24,]),'SUMA':([3,9,26,27,28,29,30,31,32,33,34,41,48,61,],[14,-34,14,-32,-25,-26,-27,-28,-29,-30,-31,14,-33,14,]),'DIVISIONENTERA':([3,9,26,27,28,29,30,31,32,33,34,41,48,61,],[15,-34,15,-32,15,15,-27,-28,-29,-30,-31,15,-33,15,]),'DIVISION':([3,9,26,27,28,29,30,31,32,33,34,41,48,61,],[16,-34,16,-32,16,16,-27,-28,-29,-30,-31,16,-33,16,]),'MULTIPLICACION':([3,9,26,27,28,29,30,31,32,33,34,41,48,61,],[17,-34,17,-32,17,17,-27,-28,-29,-30,-31,17,-33,17,]),'MODULO':([3,9,26,27,28,29,30,31,32,33,34,41,48,61,],[18,-34,18,18,18,18,18,18,18,18,-31,18,-33,18,]),'EXPONENTE':([3,9,26,27,28,29,30,31,32,33,34,41,48,61,],[19,-34,19,19,19,19,19,19,19,19,19,19,-33,19,]),'IGUAL':([5,6,22,43,44,52,65,],[20,24,42,-9,-10,-22,-23,]),'COMA':([5,35,39,40,43,45,46,49,54,62,63,67,],[23,-15,-13,-14,23,56,-11,56,56,56,56,68,]),'CORCHETEIZQ':([5,20,42,],[21,37,55,]),'PARENTESISDER':([9,26,27,28,29,30,31,32,33,34,35,39,40,47,48,69,70,],[-34,48,-32,-25,-26,-27,-28,-29,-30,-31,-15,-13,-14,57,-33,70,71,]),'CORCHETEDER':([9,27,28,29,30,31,32,33,34,35,37,39,40,41,46,48,49,50,61,62,63,],[-34,-32,-25,-26,-27,-28,-29,-30,-31,-15,-5,-13,-14,52,-11,-33,58,59,65,66,-12,]),'DOSPUNTOS':([9,27,28,29,30,31,32,33,34,41,48,],[-34,-32,-25,-26,-27,-28,-29,-30,-31,53,-33,]),'LIST':([20,],[38,]),'BOOLEAN':([20,24,37,42,55,56,68,],[40,40,40,40,40,40,40,]),'RANGE':([51,],[60,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'statements':([0,],[1,]),'statement':([0,1,],[2,12,]),'expression':([0,1,7,9,13,14,15,16,17,18,19,],[3,3,22,24,25,26,27,28,29,30,31,]),'print':([0,1,],[4,4,]),'ids':([0,1,23,],[5,5,39,]),'values':([20,40,],[32,42,]),'value':([20,40,],[33,33,]),}
+_lr_goto_items = {'statements':([0,],[1,]),'statement':([0,1,],[2,12,]),'expression':([0,1,8,10,13,14,15,16,17,18,19,21,53,],[3,3,26,27,28,29,30,31,32,33,34,41,61,]),'print':([0,1,],[4,4,]),'ids':([0,1,23,],[6,6,44,]),'index':([5,],[22,]),'value':([20,24,37,42,55,56,68,],[36,46,46,46,46,46,69,]),'params':([24,37,42,55,56,],[45,49,54,62,63,]),'empty':([37,],[50,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,28 +27,38 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> statements","S'",1,None,None,None),
-  ('statements -> statements statement','statements',2,'p_statments','parser.py',62),
-  ('statements -> statement','statements',1,'p_statments','parser.py',63),
-  ('statement -> expression','statement',1,'p_sentencia_expr','parser.py',75),
-  ('statement -> print','statement',1,'p_sentencia_expr','parser.py',76),
-  ('print -> PUNTO','print',1,'p_printVariables','parser.py',83),
-  ('statement -> ids IGUAL values','statement',3,'p_statement_assign','parser.py',91),
-  ('ids -> ID COMA ids','ids',3,'p_ids','parser.py',100),
-  ('ids -> ID','ids',1,'p_ids','parser.py',101),
-  ('values -> value COMA values','values',3,'p_values','parser.py',108),
-  ('values -> value','values',1,'p_values','parser.py',109),
-  ('value -> INT','value',1,'p_value','parser.py',156),
-  ('value -> BOOLEAN','value',1,'p_value','parser.py',157),
-  ('value -> ID','value',1,'p_value','parser.py',158),
-  ('statement -> TYPE PARENTESISIZQ ID PARENTESISDER','statement',4,'p_type','parser.py',169),
-  ('expression -> expression RESTA expression','expression',3,'p_expression_op','parser.py',203),
-  ('expression -> expression SUMA expression','expression',3,'p_expression_op','parser.py',204),
-  ('expression -> expression DIVISIONENTERA expression','expression',3,'p_expression_op','parser.py',205),
-  ('expression -> expression DIVISION expression','expression',3,'p_expression_op','parser.py',206),
-  ('expression -> expression MULTIPLICACION expression','expression',3,'p_expression_op','parser.py',207),
-  ('expression -> expression MODULO expression','expression',3,'p_expression_op','parser.py',208),
-  ('expression -> expression EXPONENTE expression','expression',3,'p_expression_op','parser.py',209),
-  ('expression -> RESTA expression','expression',2,'p_expression_uminus','parser.py',242),
-  ('expression -> PARENTESISIZQ expression PARENTESISDER','expression',3,'p_factor_expr','parser.py',248),
-  ('expression -> INT','expression',1,'p_expression_num','parser.py',253),
+  ('statements -> statements statement','statements',2,'p_statments','parser.py',59),
+  ('statements -> statement','statements',1,'p_statments','parser.py',60),
+  ('statement -> expression','statement',1,'p_sentencia_expr','parser.py',72),
+  ('statement -> print','statement',1,'p_sentencia_expr','parser.py',73),
+  ('empty -> <empty>','empty',0,'p_empty','parser.py',80),
+  ('print -> PUNTO','print',1,'p_printVariables','parser.py',87),
+  ('statement -> ID IGUAL value','statement',3,'p_statement_assign','parser.py',96),
+  ('statement -> ids IGUAL params','statement',3,'p_statement_assign','parser.py',97),
+  ('ids -> ID','ids',1,'p_ids','parser.py',109),
+  ('ids -> ID COMA ids','ids',3,'p_ids','parser.py',110),
+  ('params -> value','params',1,'p_params','parser.py',122),
+  ('params -> params COMA params','params',3,'p_params','parser.py',123),
+  ('value -> INT','value',1,'p_value','parser.py',189),
+  ('value -> BOOLEAN','value',1,'p_value','parser.py',190),
+  ('value -> ID','value',1,'p_value','parser.py',191),
+  ('statement -> TYPE PARENTESISIZQ ID PARENTESISDER','statement',4,'p_type','parser.py',202),
+  ('statement -> ID IGUAL CORCHETEIZQ params CORCHETEDER','statement',5,'p_statement_list_assign','parser.py',236),
+  ('statement -> ID IGUAL CORCHETEIZQ empty CORCHETEDER','statement',5,'p_statement_list_assign','parser.py',237),
+  ('statement -> ID index','statement',2,'p_statement_list_index','parser.py',249),
+  ('statement -> ID index IGUAL params','statement',4,'p_statement_list_index','parser.py',250),
+  ('statement -> ID index IGUAL CORCHETEIZQ params CORCHETEDER','statement',6,'p_statement_list_index','parser.py',251),
+  ('index -> CORCHETEIZQ expression CORCHETEDER','index',3,'p_statemente_dospuntos','parser.py',291),
+  ('index -> CORCHETEIZQ expression DOSPUNTOS expression CORCHETEDER','index',5,'p_statemente_dospuntos','parser.py',292),
+  ('statement -> ID IGUAL LIST PARENTESISIZQ RANGE PARENTESISIZQ INT COMA value PARENTESISDER PARENTESISDER','statement',11,'p_statement_list_range','parser.py',351),
+  ('expression -> expression RESTA expression','expression',3,'p_expression_op','parser.py',364),
+  ('expression -> expression SUMA expression','expression',3,'p_expression_op','parser.py',365),
+  ('expression -> expression DIVISIONENTERA expression','expression',3,'p_expression_op','parser.py',366),
+  ('expression -> expression DIVISION expression','expression',3,'p_expression_op','parser.py',367),
+  ('expression -> expression MULTIPLICACION expression','expression',3,'p_expression_op','parser.py',368),
+  ('expression -> expression MODULO expression','expression',3,'p_expression_op','parser.py',369),
+  ('expression -> expression EXPONENTE expression','expression',3,'p_expression_op','parser.py',370),
+  ('expression -> RESTA expression','expression',2,'p_expression_uminus','parser.py',403),
+  ('expression -> PARENTESISIZQ expression PARENTESISDER','expression',3,'p_factor_expr','parser.py',409),
+  ('expression -> INT','expression',1,'p_expression_num','parser.py',415),
 ]
