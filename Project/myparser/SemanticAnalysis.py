16401e2c52271e5a28cf9ad3a3d8108ad010809f
Heutlett <44324449+Heutlett@users.noreply.github.com>
2021-06-15 21:55:27 -0600

    se inicia el analisis semantico

---
+++ Project/myparser/SemanticAnalysis.py
@@ -0,0 +1,28 @@
+from ParserAdrian3 import result as sinaticList
+from ParserAdrian3 import errors as errorList
+
+import pprint
+
+
+print("\n--------- RESULTS ---------")
+
+pp = pprint.PrettyPrinter(indent=2)
+
+pp.pprint(sinaticList)
+
+print("\n--------- Errors ---------")
+pp.pprint(errorList)
+
+"""
+# Revisa que tod0 el código se encuentre dentro de PROCEDURES 
+def checkBlocks():
+    
+    for line in sinaticList:
+        
+
+
+
+"""
+
+
+
