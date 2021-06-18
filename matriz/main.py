845abf1b830d342ce7ecb74cedbf13a597adbc25
Jose <joseandressolanomora@gmail.com>
2021-06-18 12:01:45 -0600

    [Updated] Se adapta el controlador de envio en python

--- matriz/main.py
+++ matriz/main.py
@@ -82,21 +82,47 @@ def trans_mtrx(m):
         for j in i:
             reslt+=j.__str__()
 
-    return reslt
+    return f"{int(reslt, 2):X}"
 
 '''Envia una matriz por serial a Arduino'''
-def send_mtrx(m):
-    serialcomm.write(trans_mtrx(m).encode())
+def send_mtrx(r):
+    serialcomm.write(r.encode())
     time.sleep(0.5)
-    #print(serialcomm.readline().decode('ascii'))
+    print(serialcomm.readline().decode('ascii'))
 
+def build_send(rules_):
+    return "(" + build_aux(rules_,0) + ")"
+
+def build_aux(rules_,i):
+    try:
+        if (rules_[i][0] == 'PRINT'):
+            #print("P"+trans_mtrx(rules_[i][1]))
+            return "P"+trans_mtrx(rules_[i][1])+build_aux(rules_, i+1)
+        elif (rules_[i][0] == 'DELAY'):
+            #print("T"+rules_[i][1]+rules_[i][2].__str__())
+            return "T"+res_t(rules_[i][1])+dectohex(rules_[i][2])+build_aux(rules_, i+1)
+        else:
+            return ""
+    except:
+        return ""
+
+def res_t(time_):
+    if time_ == 'seg':
+        return "S"
+    elif time_ == 'min':
+        return "M"
+    elif time_ == 'mil':
+        return "N"
+
+def dectohex(num):
+    return f"{int(num.__str__(), 10):X}"
 
 if __name__ == '__main__':
-    time.sleep(3)
-    send_mtrx(mtrx)
-    time.sleep(0.8)
-    send_mtrx(mtrx2)
-    time.sleep(0.8)
-    send_mtrx(mtrx3)
+    time.sleep(4)
+    # send_mtrx(mtrx)
+    # time.sleep(0.8)
+    # send_mtrx(mtrx2)
+    send_mtrx(build_send(rules))
+    print(build_send(rules))
     serialcomm.close()
 
